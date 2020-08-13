75d4da1af5fe9b45f23f03a62ed5e952808ced1e
Anwesh43 <anweshthecool0@gmail.com>
2020-08-14 03:29:35 +0530

    addding constants and libraries

--- jumpingsquaretolineview/src/main/java/com/anwesh/uiprojects/jumpingsquaretolineview/JumpingSquareToLineView.kt
+++ jumpingsquaretolineview/src/main/java/com/anwesh/uiprojects/jumpingsquaretolineview/JumpingSquareToLineView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.jumpingsquaretolineview
 /**
  * Created by anweshmishra on 14/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val colors : Array<String> = arrayOf("#009688", "#F44336", "#4CAF50", "#00BCD4", "#3F51B5")
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.6f
+val delay : Long = 20
+val rot : Float = 90f
