388db9aa0161fafcfee59e3040062bf8f328a5ec
Anwesh43 <anweshthecool0@gmail.com>
2020-08-14 03:44:02 +0530

    adding JSTLNode class

--- jumpingsquaretolineview/src/main/java/com/anwesh/uiprojects/jumpingsquaretolineview/JumpingSquareToLineView.kt
+++ jumpingsquaretolineview/src/main/java/com/anwesh/uiprojects/jumpingsquaretolineview/JumpingSquareToLineView.kt
@@ -111,4 +111,44 @@ class JumpingSquareToLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JSTLNode(var i : Int, val state : State = State()) {
+
+        private var next : JSTLNode? = null
+        private var prev : JSTLNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JSTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+        fun getNext(dir : Int, cb : () -> Unit) : JSTLNode {
+            var curr : JSTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
